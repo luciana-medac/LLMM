82c4906797a3249d8e3bb9c5dc8943c5e4fc825d
lachthachi <lmg0043@alu.medac.es>
2025-04-28 09:18:41 +0200

    ejemplos tema 18 LLMM

---
+++ Lenguaje de marca/TEMA18/prueba.js
@@ -0,0 +1,5 @@
+console.log("Hola Mundo"); //en inspeccionar aparece el mensaje, esto sirve para pruebas
+
+alert("Hola Mundo"); //salta un mensaje de alerta al cargar la página
+
+document.write("Hola mundo"); //escribe directamente en la página web
