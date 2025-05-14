b0911c353c7a7424a64292a8c9a6dbf5e9d3d5c5
luciana-medac <lmg0043@alu.medac.es>
2025-05-14 19:55:03 +0200

    js2 = añadir tarea(articulo)
    terminado, se añade correctamente el articulo, muestra una alerta y además resetea correctamente los valores de los colores y demás

--- Proyecto1/javaScript/script.js
+++ Proyecto1/javaScript/script.js
@@ -2,31 +2,56 @@
 
 var boton = document.getElementById('btnArticulo');
 
-boton.addEventListener('click' , function(){
+boton.addEventListener('click', function () {
 
     //Sacamos el contenido del input y del textarea
     var input = document.getElementById('tituloArticulo').value;
     var textarea = document.getElementById('contenidoArticulo').value;
 
-    //Creamos la etiqueta articulo y la añadimos a la seccion
-    var articulo = document.createElement('article');
-    var seccion = document.getElementById('seccion');
-    seccion.appendChild(articulo);
+    // Restaurar estilo al enfocar el input o escribir
+    document.getElementById('tituloArticulo').addEventListener('focus', function () {
+        input = document.getElementById('tituloArticulo').style.backgroundColor = 'rgb(245, 234, 214)';
+    });
 
-    //Creamos las etiquetas que va a contener el articulo
-    var h3 = document.createElement('h3');
-    var p = document.createElement('p');
-    //Las añadimos a la etiqueta articulo
-    articulo.appendChild(h3);
-    articulo.appendChild(p);
+    document.getElementById('contenidoArticulo').addEventListener('focus', function () {
+        textarea = document.getElementById('contenidoArticulo').style.backgroundColor = 'rgb(245, 234, 214)';
+    });
 
-    //El contenido de input va en el titulo del articulo
-    h3.innerHTML = input;
-    //El contenido de textarea va en el párrafo del articulo
-    p.innerHTML = textarea;
 
-    articulo.id = 'blog';
+    if (input && textarea) {
+        //Creamos la etiqueta articulo y la añadimos a la seccion
+        var articulo = document.createElement('article');
+        var seccion = document.getElementById('seccionVacia')
+        seccion.appendChild(articulo)
 
-    
+        //Creamos las etiquetas que va a contener el articulo
+        var h3 = document.createElement('h3');
+        var p = document.createElement('p');
+        //Las añadimos a la etiqueta articulo
+        articulo.appendChild(h3);
+        articulo.appendChild(p);
+
+        //El contenido de input va en el titulo del articulo
+        h3.innerHTML = input;
+        //El contenido de textarea va en el párrafo del articulo
+        p.innerHTML = textarea;
+
+        articulo.className = 'articuloNuevo';
+        articulo.id = 'blog';
+        seccion.className = 'seccion3';
+
+        //Restaura los valores, vuelve a dejar vacio el input y el textarea y vuelve a su color original
+        input = document.getElementById('tituloArticulo').value = '';
+        textarea = document.getElementById('contenidoArticulo').value = '';
+        input = document.getElementById('tituloArticulo').style.backgroundColor = 'rgb(245, 234, 214)';
+        textarea = document.getElementById('contenidoArticulo').style.backgroundColor = 'rgb(245, 234, 214)';
+
+    } else {
+        //Muestra una alerta si no se completan los campos
+        alert("Debes completar todos los campos");
+        //Cambia el color
+        input = document.getElementById('tituloArticulo').style.backgroundColor = 'rgb(218, 88, 71)';
+        textarea = document.getElementById('contenidoArticulo').style.backgroundColor = ' rgb(218, 88, 71)';
+    }
 
 });
